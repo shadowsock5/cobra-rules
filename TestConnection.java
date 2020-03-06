f8f76cee284ee3494642a410a0237fec29d7421b
shadowsock5 <30398606+shadowsock5@users.noreply.github.com>
2020-03-06 18:56:33 +0800

    Update TestConnection.java

--- TestConnection.java
+++ TestConnection.java
@@ -1,18 +1,32 @@
+//import org.apache.http.client.methods.HttpGet;
+//import org.apache.http.impl.client.CloseableHttpClient;
+//import org.apache.http.impl.client.HttpClients;
+
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.net.URLConnection;
 import javax.imageio.ImageIO;
 
+/**
+* 用于测试具体发起请求的方法
+*/
+
 public class TestConnection{
         public static void main(String[] args){
                 try{
-                String url = "http://716c0ig815i2dsupouqecmfn4ea4yt.burpcollaborator.net/test_ssrf";
+                String url = "http://xjkz5jjon8wk9v736bqid3xr9if83x.burpcollaborator.net/test_ssrf";
                 URL u = new URL(url);
-                //URLConnection urlConnection = u.openConnection();  // 并不发起连接
+                URLConnection urlConnection = u.openConnection();  // 并不发起连接
                 //urlConnection.connect();                         // 发起DNS请求
                 //urlConnection.getInputStream();                  // 发起HTTP请求
+                //urlConnection.getLastModified();                   // 发起HTTP请求
+
                 //u.openStream();                                    // 发起HTTP请求
-                ImageIO.read(u);                                    // 发起DNS/HTTP请求
+                u.getContent();
+                //ImageIO.read(u);                                    // 发起DNS/HTTP请求
+                //CloseableHttpClient client = HttpClients.createDefault();
+                //HttpGet httpGet = new HttpGet(url);
+                //client.execute(httpGet); //发起请求
                 }catch(java.io.IOException e){
                         e.printStackTrace();
                 }
